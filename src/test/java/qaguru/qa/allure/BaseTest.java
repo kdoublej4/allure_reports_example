70027e6b8feb801898cbb84e1e60df956265072f
janbolot <kdoublej91@gmail.com>
2024-08-22 10:36:23 +0600

    add capabilities

--- src/test/java/qaguru/qa/allure/BaseTest.java
+++ src/test/java/qaguru/qa/allure/BaseTest.java
@@ -2,10 +2,17 @@ package qaguru.qa.allure;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class BaseTest {
     @BeforeAll
     public static void setUp() {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("chrome");
+        capabilities.setVersion("latest");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", false);
+
 //        Configuration.headless = false;
 //        Configuration.browserSize = "1920x1080";
     }
