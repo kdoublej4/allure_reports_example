7058edf22a73480d814d9d666e31a6b3bab2a19b
janbolot <kdoublej91@gmail.com>
2024-08-22 10:43:47 +0600

    on headless mode

--- src/test/java/qaguru/qa/allure/BaseTest.java
+++ src/test/java/qaguru/qa/allure/BaseTest.java
@@ -1,19 +1,21 @@
 package qaguru.qa.allure;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class BaseTest {
     @BeforeAll
     public static void setUp() {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setBrowserName("chrome");
-        capabilities.setVersion("latest");
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", false);
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setBrowserName("chrome");
+//        capabilities.setVersion("latest");
+//        capabilities.setCapability("enableVNC", true);
+//        capabilities.setCapability("enableVideo", false);
 
-//        Configuration.headless = false;
-//        Configuration.browserSize = "1920x1080";
+        Configuration.headless = false;
+        Configuration.browserSize = "1920x1080";
     }
 }
