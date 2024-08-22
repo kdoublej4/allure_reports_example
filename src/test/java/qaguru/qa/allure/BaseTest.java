5bb4838346ccd6c16d50d00385e31df548cc7bc3
janbolot <kdoublej91@gmail.com>
2024-08-22 10:31:31 +0600

    delete headless mode configuration

--- src/test/java/qaguru/qa/allure/BaseTest.java
+++ src/test/java/qaguru/qa/allure/BaseTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.BeforeAll;
 public class BaseTest {
     @BeforeAll
     public static void setUp() {
-        Configuration.headless = false;
-        Configuration.browserSize = "1920x1080";
+//        Configuration.headless = false;
+//        Configuration.browserSize = "1920x1080";
     }
 }
